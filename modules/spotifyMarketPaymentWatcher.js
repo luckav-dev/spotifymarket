2fab0febdf38821b39f0635b2ae13f94757cb185
Luckav <avluck4@gmail.com>
2026-08-17 19:03:45 +0200

    Use Spotify Market signed order links for payment CTAs

---
+++ modules/spotifyMarketPaymentWatcher.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const SellAuthPaymentWatcher = require('./sellAuthPaymentWatcher');
+const logger = require('../utils/logger');
+
+const DEFAULT_WEB_URL = 'https://spotifymarket.xyz';
+
+class SpotifyMarketPaymentWatcher extends SellAuthPaymentWatcher {
+    async invoiceUrl(invoice) {
+        const id = String(invoice?.id ?? '').trim();
+        if (id && typeof this.api.crearEnlaceFactura === 'function') {
+            try {
+                const url = await this.api.crearEnlaceFactura(id);
+                if (/^https:\/\//i.test(url)) return url;
+            } catch (error) {
+                logger.warn('sellauth:payments', `No se pudo crear el enlace privado del pedido #${id}: ${error.message}`);
+            }
+        }
+
+        const base = String(process.env.SPOTIFY_MARKET_WEB_URL || DEFAULT_WEB_URL).trim().replace(/\/+$/, '');
+        return `${base}/account`;
+    }
+}
+
+module.exports = SpotifyMarketPaymentWatcher;
